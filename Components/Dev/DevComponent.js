7d1f36fa32b81e9ee8c5f8ef757729e5d0aad388
Dylan King <xylvnking@gmail.com>
2022-10-06 16:41:37 -0400

    start screen finished

---
+++ Components/Dev/DevComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DevComponent() {
+  return (
+    <div>
+        Dylan King
+    </div>
+  )
+}
---
+++ pages/dev.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import DevComponent from '../Components/Dev/DevComponent'
+
+export default function dev() {
+  return (
+    <DevComponent />
+  )
+}
